e0fb1d1b61a552adadc88108f6cce86de98ae634
Carter McKinnon <cartermckinnon@gmail.com>
2021-02-07 02:28:07 -0800

    Release v0.1.0

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ const (
 func main() {
 	flaggy.SetName("url-monitor")
 	flaggy.SetDescription("Monitors URLs for patterns, and alerts by SMS.")
-	flaggy.SetVersion("0.1.0-dev")
+	flaggy.SetVersion("0.1.0")
 
 	var configurationFile = "configuration.yaml"
 	flaggy.String(&configurationFile, "c", "configuration-file", "YAML configuration file defining URLs to monitor")
